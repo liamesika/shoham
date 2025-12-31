01c1522368e79aefbe6946ec9d6460c9de948cc1
liamesika <liamesika2121@gmail.com>
2025-12-31 20:39:31 +0200

    lia

--- IsSorted.java
+++ IsSorted.java
@@ -1,21 +1,19 @@
 public class IsSorted {
 
-    // Public function: called by the user
     public static boolean isSorted(int[] a) {
-        return isSorted(a, 0);
+        return isSorted(a, 0, a.length - 1);
     }
 
-    // Helper recursive function
-    private static boolean isSorted(int[] a, int i) {
-        //// Replace the following statement with your code
-        return false; 
-    }
+    private static boolean isSorted(int[] a, int i, int last) {
 
-    public static void main(String[] args) {
-        int[] a = {1, 2, 2, 5};
-        System.out.println(isSorted(a)); // true
+        if (i >= last) {
+            return true;
+        }
 
-        int[] b = {1, 3, 2};
-        System.out.println(isSorted(b)); // false
+        if (a[i] > a[i + 1]) {
+            return false;
+        }
+
+        return isSorted(a, i + 1, last);
     }
 }
